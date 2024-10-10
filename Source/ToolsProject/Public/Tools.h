4356aa393679ffd740e4cd09d40338398b353044
VaeDeveloper <dmitriyalexdev@gmail.com>
2024-10-10 17:49:31 +0300

    add project source files, for compilation plugin

---
+++ Source/ToolsProject/Public/Tools.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * emtpy class for compilation plugin project 
+ */
+class TOOLSPROJECT_API Tools
+{
+public:
+	Tools();
+	~Tools();
+};
