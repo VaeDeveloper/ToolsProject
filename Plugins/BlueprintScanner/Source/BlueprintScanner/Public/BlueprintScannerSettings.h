4f782399a09e5370ab5d9ad9d75b3b96c7a6fb59
VaeDeveloper <dmitriyalexdev@gmail.com>
2024-12-10 22:57:21 +0300

    Add Blueprint Scanner Developer Settings in plugin

---
+++ Plugins/BlueprintScanner/Source/BlueprintScanner/Public/BlueprintScannerSettings.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettings.h"
+#include "BlueprintScannerSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLUEPRINTSCANNER_API UBlueprintScannerSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+	
+};
