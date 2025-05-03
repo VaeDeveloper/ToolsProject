0ec0eb039b63a48525aabe1b4d119084d9d64138
Dmitriy Alexandrov <104723129+VaeDeveloper@users.noreply.github.com>
2025-05-03 20:24:28 +0300

    deleting a macro because it is no longer needed

---
+++ enc_temp_folder/9a7addf0cf12fb4c1988eb177f41fab/DataAssetManager.cpp
@@ -0,0 +1,120 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DataAssetManager.h"
+#include "Modules/ModuleManager.h"
+
+#if WITH_EDITOR
+#include "Editor/EditorEngine.h"
+#include "StatusBarSubsystem.h"
+#include "DeveloperSettings/DataAssetManagerSettings.h"
+#include "UI/SDataAssetManagerWidget.h"
+#include "UI/SDeveloperSettingsWidget.h"
+#endif // WITH_EDITOR
+
+const FName FDataAssetManagerModule::DataAssetManagerTabName = FName("DataAssetManager");
+
+#define LOCTEXT_NAMESPACE "FDataAssetManagerModule"
+
+namespace DataAssetManager
+{
+	namespace ModuleName
+	{
+		const FName ToolProjectEditor(TEXT("ToolProjectEditor"));
+	}
+
+	constexpr float TabReopenDelaySeconds = 1.0f;
+	const FName StatusBarName(TEXT("DataAssetManagerStatusBar"));
+}
+
+void FDataAssetManagerModule::StartupModule()
+{
+#if WITH_EDITOR
+	FPropertyEditorModule& PropertyEditorModule = FModuleManager::LoadModuleChecked<FPropertyEditorModule>(DataAssetManager::ModuleName::PropertyEditor);
+	PropertyEditorModule.RegisterCustomClassLayout(UDataAssetManagerSettings::StaticClass()->GetFName(),
+	FOnGetDetailCustomizationInstance::CreateStatic(&SDeveloperSettingsWidget::MakeInstance));
+#endif
+	FGlobalTabmanager::Get()->RegisterNomadTabSpawner(
+	DataAssetManagerTabName,
+	FOnSpawnTab::CreateRaw(this, &FDataAssetManagerModule::CreateDataAssetManagerTab))
+	.SetDisplayName(LOCTEXT("FDataAssetManagerModule", "Data Asset Manager"))
+	.SetMenuType(GetVisibleModule());
+}
+
+void FDataAssetManagerModule::ShutdownModule()
+{
+#if WITH_EDITOR
+	if (FModuleManager::Get().IsModuleLoaded(DataAssetManager::ModuleName::PropertyEditor))
+	{
+		FPropertyEditorModule& PropertyEditorModule = FModuleManager::GetModuleChecked<FPropertyEditorModule>(DataAssetManager::ModuleName::PropertyEditor);
+		PropertyEditorModule.UnregisterCustomClassLayout(UDataAssetManagerSettings::StaticClass()->GetFName());
+	}
+#endif
+}
+
+ETabSpawnerMenuType::Type FDataAssetManagerModule::GetVisibleModule() const
+{
+	if (FModuleManager::Get().IsModuleLoaded(DataAssetManager::ModuleName::ToolProjectEditor))
+	{
+		return ETabSpawnerMenuType::Enabled;
+	}
+	return ETabSpawnerMenuType::Hidden;
+}
+
+TSharedRef<SDockTab> FDataAssetManagerModule::CreateDataAssetManagerTab(const FSpawnTabArgs& Args)
+{
+	TSharedRef<SDockTab> DataAssetManagerTab = SNew(SDockTab).TabRole(ETabRole::NomadTab);
+#if WITH_EDITOR
+	UStatusBarSubsystem* StatusBarSubsystem = GEditor->GetEditorSubsystem<UStatusBarSubsystem>();
+	if(StatusBarSubsystem)
+	{
+		TSharedRef<SWidget> StatusBarWidget = StatusBarSubsystem->MakeStatusBarWidget(DataAssetManager::StatusBarName, DataAssetManagerTab);
+
+		DataAssetManagerTab->SetContent(
+			SNew(SVerticalBox)
+			+ SVerticalBox::Slot()
+			.FillHeight(1.0f)
+			[
+				SNew(SDataAssetManagerWidget)
+			]
+			+ SVerticalBox::Slot()
+			.AutoHeight()
+			[
+				StatusBarWidget
+			]
+		);
+	}
+	else
+	{
+		DataAssetManagerTab->SetContent(SNew(SDataAssetManagerWidget));
+	}
+#else
+	DataAssetManagerTab->SetContent(SNew(SDataAssetManagerWidget));
+#endif
+
+	return DataAssetManagerTab;
+}
+
+void FDataAssetManagerModule::RestartWidget()
+{
+	TSharedPtr<SDockTab> DataAssetManagerTab = FGlobalTabmanager::Get()->FindExistingLiveTab(DataAssetManagerTabName);
+	if (DataAssetManagerTab.IsValid())
+	{
+		DataAssetManagerTab->RequestCloseTab();
+
+		// Set timer to reopen the tab after a short delay.
+		FTimerHandle TimerHandle;
+		GEditor->GetTimerManager().Get().SetTimer(TimerHandle, []()
+			{
+				FGlobalTabmanager::Get()->TryInvokeTab(DataAssetManagerTabName);
+			}, DataAssetManager::TabReopenDelaySeconds, false);
+	}
+}
+
+void FDataAssetManagerModule::OpenDataAssetManagerTab() 
+{
+    FGlobalTabmanager::Get()->TryInvokeTab(DataAssetManagerTabName);
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FDataAssetManagerModule, DataAssetManager)
