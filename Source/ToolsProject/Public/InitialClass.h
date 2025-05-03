35763701c1be22b55e1d364beab76debc101306c
Dmitriy Alexandrov <104723129+VaeDeveloper@users.noreply.github.com>
2025-05-03 16:42:13 +0300

    Initial commit

---
+++ Source/ToolsProject/Public/InitialClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class TOOLSPROJECT_API InitialClass
+{
+public:
+	InitialClass();
+	~InitialClass();
+};
