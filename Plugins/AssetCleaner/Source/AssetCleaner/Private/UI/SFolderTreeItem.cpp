54dec06e2d3c36f50b994980b28ef75041532ba2
Dmitriy Alexandrov <104723129+VaeDeveloper@users.noreply.github.com>
2025-05-23 15:15:21 +0300

    Create SFolderTreeItem class and move all logic in this class

---
+++ Plugins/AssetCleaner/Source/AssetCleaner/Private/UI/SFolderTreeItem.cpp
@@ -0,0 +1,188 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/SFolderTreeItem.h"
+
+void SFolderItemTree::Construct(const FArguments& InArgs, const TSharedRef<STableViewBase>& InTable)
+{
+	Item = InArgs._Item;
+	HighlightText = InArgs._HightlightText;
+
+	SMultiColumnTableRow::Construct(SMultiColumnTableRow::FArguments().Padding(FMargin{ 0.0f, 2.0f, 0.0f, 0.0f }), InTable);
+}
+
+
+TSharedRef<SWidget> SFolderItemTree::GenerateWidgetForColumn(const FName& InColumnName) 
+{
+	if(InColumnName.IsEqual(TEXT("Path")))
+	{
+		return
+			SNew(SHorizontalBox)
+			.ToolTipText(FText::FromString(Item->FolderPath))
+
+			+ SHorizontalBox::Slot()
+			.AutoWidth()
+			.Padding(FMargin{ 2.0f })
+			[
+				SNew(SExpanderArrow, SharedThis(this))
+					.IndentAmount(10)
+					.ShouldDrawWires(false)
+			]
+			+ SHorizontalBox::Slot()
+			.AutoWidth()
+			.Padding(0, 0, 2, 0)
+			.VAlign(VAlign_Center)
+			[
+				SNew(SImage)
+				.Image(GetFolderIcon())
+				.ColorAndOpacity(GetFolderColor())
+			]
+			+ SHorizontalBox::Slot()
+			.AutoWidth()
+			.Padding(FMargin{ 2.0f })
+			[
+				SNew(STextBlock)
+				.Text(FText::FromString(Item->FolderName))
+				.HighlightText(HighlightText)
+			];
+	}
+
+	if(InColumnName.IsEqual(TEXT("NumAssetsTotal")))
+	{
+		return
+			SNew(SHorizontalBox)
+			+ SHorizontalBox::Slot().FillWidth(1.0f).HAlign(HAlign_Center).VAlign(VAlign_Center)
+			[
+				SNew(STextBlock)
+				.AutoWrapText(false)
+				.Justification(ETextJustify::Center)
+				.ColorAndOpacity(FLinearColor::White)
+				.Text(FText::AsNumber(Item->NumAssetsTotal))
+			];
+	}
+
+	if(InColumnName.IsEqual(TEXT("NumAssetsUsed")))
+	{
+		return
+			SNew(SHorizontalBox)
+			+ SHorizontalBox::Slot()
+			.FillWidth(1.0f)
+			.HAlign(HAlign_Center)
+			.VAlign(VAlign_Center)
+			[
+				SNew(STextBlock)
+				.AutoWrapText(false)
+				.Justification(ETextJustify::Center)
+				.ColorAndOpacity(FLinearColor::White)
+				.Text(FText::AsNumber(Item->NumAssetsUsed))
+			];
+	}
+
+	if(InColumnName.IsEqual(TEXT("NumAssetsUnused")))
+	{
+		return
+			SNew(SHorizontalBox)
+
+			+ SHorizontalBox::Slot()
+			.FillWidth(1.0f)
+			.HAlign(HAlign_Center)
+			.VAlign(VAlign_Center)
+			[
+				SNew(STextBlock)
+				.AutoWrapText(false)
+				.Justification(ETextJustify::Center)
+				.ColorAndOpacity(FLinearColor::White)
+				.Text(FText::AsNumber(Item->NumAssetsUnused))
+			];
+	}
+
+	if(InColumnName.IsEqual(TEXT("UnusedPercent")))
+	{
+		FNumberFormattingOptions FormatOptions;
+		FormatOptions.UseGrouping = true;
+		FormatOptions.MinimumFractionalDigits = 2;
+		FormatOptions.MaximumFractionalDigits = 2;
+
+		const FString UnusedStrPercent = FText::AsNumber(Item->PercentageUnused, &FormatOptions).ToString() + TEXT(" %");
+
+		return
+			SNew(SHorizontalBox)
+
+			+ SHorizontalBox::Slot()
+			.Padding(FMargin{ 5.0f, 1.0f })
+			.FillWidth(1.0f)
+			[
+				SNew(SOverlay)
+
+				+ SOverlay::Slot()
+				.HAlign(HAlign_Fill)
+				.VAlign(VAlign_Fill)
+				[
+					SNew(SProgressBar)
+					.BorderPadding(FVector2D{ 0.0f, 0.0f })
+					.Percent(Item->PercentageUnusedNormalized)
+					.Style(&FAppStyle::Get().GetWidgetStyle<FProgressBarStyle>("ProgressBar"))
+					
+					// TODO !!! 	
+					//.FillColorAndOpacity(FAppStyle::Get().GetSlateColor("Error")) 
+					//.BackgroundImage(FAppStyle::Get().GetBrush("ProgressBar.Background"))
+					//.FillImage(FAppStyle::Get().GetBrush("ProgressBar.Fill"))
+					//.FillColorAndOpacity(FAppStyle::Get().GetSlateColor("Error"))
+				]
+
+				+ SOverlay::Slot()
+				.HAlign(HAlign_Center)
+				.VAlign(VAlign_Center)
+				[
+					SNew(STextBlock)
+					.AutoWrapText(false)
+					.ColorAndOpacity(FLinearColor::White)
+					.Text(FText::FromString(UnusedStrPercent))
+				]
+			];
+	}
+
+	if(InColumnName.IsEqual(TEXT("UnusedSize")))
+	{
+		return
+			SNew(SHorizontalBox)
+			+ SHorizontalBox::Slot().Padding(FMargin{ 5.0f, 1.0f }).FillWidth(1.0f)
+			[
+				SNew(STextBlock)
+				.AutoWrapText(false)
+				.ColorAndOpacity(FLinearColor::White)
+				.Justification(ETextJustify::Center)
+				.ColorAndOpacity(FLinearColor::White)
+				.Text(FText::AsMemory(Item->SizeAssetsUnused, IEC))
+			];
+	}
+
+	return SNew(STextBlock)
+		.Text(FText::FromString(TEXT("")));
+}
+
+
+const FSlateBrush* SFolderItemTree::GetFolderIcon() const
+{
+	return FAppStyle::GetBrush(Item->bIsExpanded ? TEXT("ContentBrowser.AssetTreeFolderOpen") : TEXT("ContentBrowser.AssetTreeFolderClosed"));
+}
+
+FSlateColor SFolderItemTree::GetFolderColor() const
+{
+	if(Item->bIsExcluded)
+	{
+		return FAppStyle::Get().GetSlateColor("Warning");
+	}
+
+	if(Item->bIsDev)
+	{
+		return FAppStyle::Get().GetSlateColor("AccentBlue");
+	}
+
+	if(Item->bIsEmpty)
+	{
+		return FAppStyle::Get().GetSlateColor("Error");
+	}
+
+	return FAppStyle::Get().GetSlateColor("AccentBlue");
+}
---
+++ Plugins/AssetCleaner/Source/AssetCleaner/Public/UI/SFolderTreeItem.h
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AssetCleanerTypes.h"
+#include "Widgets/SCompoundWidget.h"
+#include "Widgets/Notifications/SProgressBar.h"
+
+/**
+ * A single row widget used to represent a folder item in an asset tree view.
+ * Implements multi-column support for folder display within an STreeView.
+ */
+class SFolderItemTree final : public SMultiColumnTableRow<TSharedPtr<SFolderItemTree>>
+{
+public:
+	SLATE_BEGIN_ARGS(SFolderItemTree) {}
+		/** The data model representing this folder node. */
+		SLATE_ARGUMENT(TSharedPtr<FAssetTreeFolderNode>, Item)
+
+		/** The text to highlight within the folder name, typically used for search filtering. */
+		SLATE_ARGUMENT(FText, HightlightText)
+	SLATE_END_ARGS()
+
+	/**
+	 * Constructs the folder row widget using the specified arguments and owning table.
+	 *
+	 * @param InArgs Construction arguments passed via SLATE macros.
+	 * @param InTable Reference to the parent table view widget.
+	 */
+	void Construct(const FArguments& InArgs, const TSharedRef<STableViewBase>& InTable);
+
+	/**
+	 * Generates the widget to use for the specified column name.
+	 * Used by STreeView to populate each column in the row.
+	 *
+	 * @param InColumnName The name of the column for which to generate content.
+	 * @return The widget representing the column's content.
+	 */
+	virtual TSharedRef<SWidget> GenerateWidgetForColumn(const FName& InColumnName) override;
+
+private:
+	/**
+	 * Gets the appropriate icon to use for this folder item.
+	 *
+	 * @return A pointer to the Slate brush representing the folder icon.
+	 */
+	const FSlateBrush* GetFolderIcon() const;
+
+	/**
+	 * Gets the color used to tint this folder item.
+	 * This can be used to visually distinguish different folder states.
+	 *
+	 * @return A Slate color representing the folder's display tint.
+	 */
+	FSlateColor GetFolderColor() const;
+
+	/** The text to be highlighted within the folder label. */
+	FText HighlightText;
+
+	/** The underlying data representing this folder item. */
+	TSharedPtr<FAssetTreeFolderNode> Item;
+};
