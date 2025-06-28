78d713e3987860975404098a65fdcfe49abe76e5
Dmitriy Alexandrov <104723129+VaeDeveloper@users.noreply.github.com>
2025-06-28 05:56:05 +0300

    add DeadBranchValidator  in ValidatorX plugin

---
+++ Plugins/ValidatorX/Source/ValidatorX/Private/Validators/DeadBranchValidator.cpp
@@ -0,0 +1,5 @@
+ся // Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Validators/DeadBranchValidator.h"
+
---
+++ Plugins/ValidatorX/Source/ValidatorX/Public/Validators/DeadBranchValidator.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseClasses/BlueprintValidatorBase.h"
+#include "DeadBranchValidator.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALIDATORX_API UDeadBranchValidator : public UBlueprintValidatorBase
+{
+	GENERATED_BODY()
+	
+};
